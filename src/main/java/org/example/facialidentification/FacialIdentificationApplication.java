5bf50e41670da0151b1af8679425412569d19fbd
Alex <visibleone@gmail.com>
2025-06-05 09:02:17 +0300

    Init project

---
+++ src/main/java/org/example/facialidentification/FacialIdentificationApplication.java
@@ -0,0 +1,13 @@
+package org.example.facialidentification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacialIdentificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacialIdentificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/facialidentification/FacialIdentificationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.facialidentification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacialIdentificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
